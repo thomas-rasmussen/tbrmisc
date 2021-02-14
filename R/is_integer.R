51b0c95ae2f9454383526366f11c8108b8fa6397
Thomas Bøjer Rasmussen <thomasboejer@gmail.com>
2021-02-14 19:08:47 +0100

    Setup package and add is_integer fct

---
+++ R/is_integer.R
@@ -0,0 +1,15 @@
+#' Check if numeric
+#'
+#' Check if each element of a numeric vector can be interpreted as integer
+#' numbers.
+#'
+#' @param x a numeric vector
+#' @param tol tolerance
+#'
+  #' @return logical vector
+#' @export
+#'
+#' @examples
+is_integer <- function(x, tol = .Machine$double.eps^0.5){
+  abs(x - round(x)) < tol
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(tbrmisc)
+
+test_check("tbrmisc")
