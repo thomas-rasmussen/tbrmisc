72e79fabcc7764c1cc5d87791c013011bea4a9d1
Thomas Bøjer Rasmussen <thomasboejer@gmail.com>
2021-09-20 09:18:07 +0200

    Update is_integer
    Add numeric check of input vector, and update tests

--- R/is_integer.R
+++ R/is_integer.R
@@ -1,4 +1,4 @@
-#' Check if numeric
+#' Check if integer(ish)
 #'
 #' Check if each element of a numeric vector can be interpreted as integer
 #' numbers.
@@ -6,12 +6,15 @@
 #' @param x a numeric vector
 #' @param tol tolerance
 #'
-  #' @return logical vector
+#' @return logical vector
 #' @export
 #'
 #' @examples
 #' is_integer(1)
 #' is_integer(1.1)
 is_integer <- function(x, tol = .Machine$double.eps^0.5){
+  if(!is.numeric(x)) {
+    stop("'x' must be numeric", call. = FALSE)
+  }
   abs(x - round(x)) < tol
 }
--- tests/testthat/test-is_integer.R
+++ tests/testthat/test-is_integer.R
@@ -1,17 +1,22 @@
-test_that("is_integer works", {
+test_that("non-numeric input triggers error", {
+  expect_error(is_integer("1"))
+  expect_error(is_integer(FALSE))
+})
+
+test_that("integer(ish) input work", {
   expect_equal(is_integer(1), TRUE)
-  expect_equal(is_integer(1L), TRUE)
   expect_equal(is_integer(1.0), TRUE)
   expect_equal(is_integer(-1), TRUE)
+})
+
+test_that("various tests", {
+  expect_equal(is_integer(1L), TRUE)
   expect_equal(is_integer(-1.5), FALSE)
-  expect_equal(is_integer(FALSE), TRUE)
-  expect_equal(is_integer(TRUE), TRUE)
-  expect_error(is_integer("1"), "non-numeric argument to mathematical function")
-  expect_equal(is_integer(c(TRUE, -4, 2.0, 6.6)), c(TRUE, TRUE, TRUE, FALSE))
+  expect_equal(is_integer(c(-4, 2.0, 6.6)), c(TRUE, TRUE, FALSE))
 
 })
 
 test_that("return obejct is logical vector", {
   expect_equal(typeof(is_integer(1)), "logical")
-  expect_equal(typeof(is_integer(c(TRUE, -4, 2.0, 6.6))), "logical")
+  expect_equal(typeof(is_integer(c(-4, 2.0, 6.6))), "logical")
 })
